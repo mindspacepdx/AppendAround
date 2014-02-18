32742e47b9125b8edf0cc7d70f4c4da4d8d56049
Jay Contonio <jcontonio@gmail.com>
2014-02-18 13:51:32 -0800

    Update appendAround.js

--- appendAround.js
+++ appendAround.js
@@ -1,42 +1,48 @@
 /*! appendAround markup pattern. [c]2012, @scottjehl, Filament Group, Inc. MIT/GPL 
 how-to:
-	1. Insert potential element containers throughout the DOM
-	2. give each container a data-set attribute with a value that matches all other containers' values
-	3. Place your appendAround content in one of the potential containers
-	4. Call appendAround() on that element when the DOM is ready
+1. Insert potential element containers throughout the DOM
+2. give each container a data-set attribute with a value that matches all other containers' values
+3. Place your appendAround content in one of the potential containers
+4. Call appendAround() on that element when the DOM is ready
 */
 (function( $ ){
-	$.fn.appendAround = function(){
-	  return this.each(function(){
-      
-	    var $self = $( this ),
-	        att = "data-set",
-	        $parent = $self.parent(), 
-	        parent = $parent[ 0 ],
-	        attval = $parent.attr( att ),
-	        $set = $( "["+ att +"='" + attval + "']" );
+	$.fn.appendAround = function() {
 
-		function isHidden( elem ){
-			return $(elem).css( "display" ) === "none";
-		}
+		// Rewrote this because it was looking at parent elements (wha?!), instead
+		// we just use the elements contents that we're showing/hiding. - jcontonio
+	
+		return this.each(function() {
 
-		function appendToVisibleContainer(){
-			if( isHidden( parent ) ){
-				var found = 0;
-				$set.each(function(){
-					if( !isHidden( this ) && !found ){
-						$self.appendTo( this );
-						found++;
-						parent = this;
-					}
-				});
-	      	}
-	    }
-      
-	    appendToVisibleContainer();
-      
-	    $(window).bind( "resize", appendToVisibleContainer );
-      
-	  });
+			var tm,
+					$self = $(this),
+					attval = $self.attr('data-set'),
+					$set = $("[data-set='" + attval + "']");
+
+			function isHidden(elem) {
+				return $(elem).css( "display" ) === "none";
+			}
+
+			function appendToVisibleContainer() {
+				clearTimeout(tm);
+				if(isHidden($self)) {
+					tm = setTimeout(function() {
+						var found = 0;
+						$set.each(function() {
+							if(!isHidden(this) && !found) {
+								$(this).html($self.html());
+								found++;
+							}
+						});
+					}, 800);
+				}
+			}
+
+			appendToVisibleContainer();
+
+			$(window).bind("resize", appendToVisibleContainer);
+
+		});
+
 	};
-}( jQuery ));
+
+}(jQuery));
